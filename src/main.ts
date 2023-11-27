f4a2666fc31d540fbaed2e55ed0ae33bfaa12fec
soheib <soheib.mohammadi95@gmail.com>
2023-11-27 17:52:24 +0330

    update port

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   });
 
   const configService = app.get(ConfigService);
-  const port = 3000;
+  const port = 3001;
 
   await app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
